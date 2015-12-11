b44c345b40311657dd92acdfcdd23b0761e9baa1
Aleksei Danilov <danilow.aleksei@gmail.com>
2015-12-11 23:56:46 +0300

    add process errors

--- src/com/epam/javauniversity/hw6/operation/Division.java
+++ src/com/epam/javauniversity/hw6/operation/Division.java
@@ -4,6 +4,9 @@ public class Division implements Operation {
 
     @Override
     public int calculate(int leftOperand, int rightOperand) {
+        if (rightOperand == 0) {
+            throw new IllegalArgumentException("right operand is 0, can not divide by 0");
+        }
         return leftOperand / rightOperand;
     }
 }
